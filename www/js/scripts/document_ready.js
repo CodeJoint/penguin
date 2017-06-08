7378d18e899d45a9256a753ea74dd7055ecc25f0
John Falcon <john@pixel-muffin.com>
2017-06-08 12:36:33 -0500

    Merge ui with endpoint integration

--- www/js/scripts/document_ready.js
+++ www/js/scripts/document_ready.js
@@ -197,9 +197,55 @@ window.initializeEvents = function(){
 		});
 
 		var ventana = $(window).height();
+
 		$('.container').css('min-height', ventana+'px');
 
-	
+		/*** FILTROS ***/
+
+		$('.filtros_wrapper').hide();
+
+		$('.header_filtros').on('click', function(){
+			if($('.filtros_wrapper').hasClass('filtros_show')){
+				$('.filtros_wrapper').removeClass('filtros_show');
+				$('.filtros_wrapper').hide();
+				console.log('success');
+			} else {
+				$('.filtros_wrapper').addClass('filtros_show');
+				$('.filtros_wrapper').show();
+			}
+		});
+
+		$('.filtros ul li').on('click', function(){
+			$('.filtros ul li').removeClass('selected');
+			var dataFilter = $(this).attr('data');
+			$(this).addClass('selected');
+			console.log(dataFilter);
+		});
+
+
+		var positiveMargin = false;
+		$('.misquinielas').on('click', function(){
+
+		    if (!positiveMargin) {
+		        var left = "0%";
+		        var padd = "1%";
+		        positiveMargin = true;
+		    }
+		    else {
+		        var left = "97%";
+		        var padd = "1%";
+		        positiveMargin = false;
+		    }
+		     $('.misquinielas').animate({
+		               marginLeft: left,
+		               paddingLeft: padd,
+		               }, {
+		               duration: 500,
+		               
+		               complete: function () {
+		               }
+		             });
+		 });
 	});
 
 }

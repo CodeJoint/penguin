f4485d8b5145e09282e88bc3932851a9b758d44b
Pablo Covarrubias <cova@Pablos-iMac.local>
2017-06-08 00:33:11 -0500

    filtros

--- www/maqueta/js/functions.js
+++ www/maqueta/js/functions.js
@@ -6,4 +6,27 @@ $(document).ready(function(){
 
 	$('.container').css('min-height', ventana+'px');
 
+	/*** FILTROS ***/
+
+	$('.filtros_wrapper').hide();
+
+	$('.header_filtros').on('click', function(){
+		if($('.filtros_wrapper').hasClass('filtros_show')){
+			$('.filtros_wrapper').removeClass('filtros_show');
+			$('.filtros_wrapper').hide();
+			console.log('success');
+		} else {
+			$('.filtros_wrapper').addClass('filtros_show');
+			$('.filtros_wrapper').show();
+		}
+	});
+
+	$('.filtros ul li').on('click', function(){
+		$('.filtros ul li').removeClass('selected');
+		var dataFilter = $(this).attr('data');
+		$(this).addClass('selected');
+		console.log(dataFilter);
+	});
+
+	
 });
